044fcf11ba61ae1b48c2413a50ded266af18363f
Furkan <furkhanbircan@gmail.com>
2022-10-13 16:24:13 +0300

    Code Refactoring and category structure has been improved

--- src/app/services/productService/product.service.ts
+++ src/app/services/productService/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductResponseModel } from '../models/product/productResponseModel';
-import { Product } from '../models/product/product';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { Product } from 'src/app/models/product/product';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +10,8 @@ import { Observable } from 'rxjs';
 export class ProductService {
   apiUrl = 'https://localhost:44317/api/products/getall';
   constructor(private httpClient: HttpClient) {}
-  
-  getProducts(): Observable<ProductResponseModel> {
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+
+  getProducts(): Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
   }
 }
